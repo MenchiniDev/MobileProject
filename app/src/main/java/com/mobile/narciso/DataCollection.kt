fa6a5276c5a3122b685507baae75ea4255504347
MenchiniDev <107705177+MenchiniDev@users.noreply.github.com>
2024-04-28 22:18:14 +0200

    minor changes and bug fixes

--- app/src/main/java/com/mobile/narciso/DataCollection.kt
+++ app/src/main/java/com/mobile/narciso/DataCollection.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.mobile.narciso.databinding.FragmentDatacollectionBinding
 
@@ -32,7 +33,8 @@ class DataCollection : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.gotoDataTesting.setOnClickListener {
+        binding.gotoDataTestingDATACOLLECTION.setOnClickListener {
+            Toast.makeText(requireContext(), "sto andando a data testing!", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_DataCollection_to_DataTesting)
         }
     }
--- app/src/main/java/com/mobile/narciso/DataTesting.kt
+++ app/src/main/java/com/mobile/narciso/DataTesting.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mobile.narciso.databinding.FragmentDatatestingBinding
@@ -30,6 +31,7 @@ class DataTesting : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.gotoDataCollection.setOnClickListener {
+            Toast.makeText(requireContext(), "sto andando a data collection!", Toast.LENGTH_SHORT).show()
             findNavController().navigate(R.id.action_DataTesting_to_DataCollection)
         }
     }
