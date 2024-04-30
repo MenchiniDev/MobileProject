ef840e8640857a22cedd7cdaa5e6bd6de7b62402
MenchiniDev <107705177+MenchiniDev@users.noreply.github.com>
2024-04-30 09:20:32 +0200

    added camera page, doesn't takes photos yet

--- app/src/main/java/com/mobile/narciso/DataCollection.kt
+++ app/src/main/java/com/mobile/narciso/DataCollection.kt
@@ -1,13 +1,20 @@
 package com.mobile.narciso
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.mobile.narciso.databinding.FragmentDatacollectionBinding
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
@@ -15,10 +22,16 @@ import com.mobile.narciso.databinding.FragmentDatacollectionBinding
 class DataCollection : Fragment() {
 
     private var _binding: FragmentDatacollectionBinding? = null
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
+
+    private val CAMERA_PERMISSION_CODE = 1001
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,17 +39,32 @@ class DataCollection : Fragment() {
     ): View? {
 
         _binding = FragmentDatacollectionBinding.inflate(inflater, container, false)
-        return binding.root
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Permission is already granted
+            // Proceed with camera operations
+            // E.g., openCamera()
+        } else {
+            // Request camera permission
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+        }
 
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.gotoDataTestingDATACOLLECTION.setOnClickListener {
-            Toast.makeText(requireContext(), "sto andando a data testing!", Toast.LENGTH_SHORT).show()
+            Toast.makeText(requireContext(), "sto andando a data testing!", Toast.LENGTH_SHORT)
+                .show()
             findNavController().navigate(R.id.action_DataCollection_to_DataTesting)
         }
+
+        binding.gotoCamera.setOnClickListener {
+            findNavController().navigate(R.id.action_DataCollection_to_camera)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
     }
 
     override fun onDestroyView() {
