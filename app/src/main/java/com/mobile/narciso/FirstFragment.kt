88449441012cfe48ffd72cb3db5105c6e1793512
MenchiniDev <107705177+MenchiniDev@users.noreply.github.com>
2024-05-05 10:23:45 +0200

    added logout, added some aestetic upgradings (more coming soon), mancano forgot password e visualizzazione dati ottenuti

--- app/src/main/java/com/mobile/narciso/FirstFragment.kt
+++ app/src/main/java/com/mobile/narciso/FirstFragment.kt
@@ -1,34 +1,69 @@
 package com.mobile.narciso
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.mobile.narciso.databinding.FragmentFirstBinding
 
-/**
- * A simple [Fragment] subclass as the default destination in the navigation.
- */
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val REQUEST_ENABLE_BT = 1
+    private val REQUEST_CAMERA_PERMISSION = 2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
 
-        
-        binding.textviewFirst.text= "Welcome to future, " + SessionManager(requireContext()).username
+        binding.textviewFirst.text = getString(R.string.presentation, SessionManager(requireContext()).username)
 
+        binding.gotoDataCollection.isEnabled = false
+        // Check Bluetooth connection
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            binding.textviewWatchOK.setTextColor(Color.RED)
+        }else
+        {
+            binding.textviewWatchOK.setTextColor(Color.GREEN)
+        }
+
+        // Check camera permission
+        if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA_PERMISSION)
+            binding.textviewCamOK.setTextColor(Color.RED)
+        }else
+        {
+            binding.textviewCamOK.setTextColor(Color.GREEN)
+        }
+
+        // Set helmet textview to red by default
+        binding.textviewHelmetOK.setTextColor(Color.RED)
+
+        // Set radio button listener
+        binding.radioButton.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                binding.textviewHelmetOK.setTextColor(Color.GREEN)
+                binding.gotoDataCollection.isEnabled = true
+            } else {
+                binding.textviewHelmetOK.setTextColor(Color.RED)
+                binding.gotoDataCollection.isEnabled = false
+            }
+        }
 
         return binding.root
     }
@@ -36,9 +71,6 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.gotoDataTesting.setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_DataTesting)
-        }
         binding.gotoDataCollection.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_DataCollection)
         }
